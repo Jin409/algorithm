ef217fa2e4acfc81bf0ed2ad9c0a193ba2b86e62
seunghee jin <sualng123@gmail.com>
2022-03-20 18:59:12 +0900

    5-2-3

--- CT/NM/5-2-3.py
+++ CT/NM/5-2-3.py
@@ -1,11 +1,16 @@
+#틀렸던 이유 : 증가를 고려하지 않음. 
+
 n,m = map(int,input().split())
-arr1 = [0]*1000 #긱 인덱스는 시간!
-arr2 = [0]*1000
+arr1 = [0]*1000000 #긱 인덱스는 시간!
+arr2 = [0]*1000000
+
 def move1(s,v,t):
     #s는 시작점 v는 속도 t는 시간
     idx = s
     for i in range(t):
-        arr1[idx]=v
+        #이전에는
+        #arr1[idx]=v 여서 틀림.
+        arr1[idx]=arr1[idx-1]+v
         idx+=1
     return idx
     
@@ -19,7 +24,7 @@ def move2(s,v,t):
     #s는 시작점 v는 속도 t는 시간
     idx = s
     for i in range(t):
-        arr2[idx]=v
+        arr2[idx]=arr2[idx-1]+v
         idx+=1
     return idx
 
@@ -29,20 +34,23 @@ for i in range(m):
     v,t = map(int,input().split())
     now = move2(now,v,t)
 
+cnt=0
+first = 0
+
 def check(i):
+    global first
     if arr1[i]>arr2[i]:
         first = 1
     elif arr1[i]<arr2[i]:
         first = 2
+    else:
+        first = first
     return first
 
 
 
-cnt=0
-first = 0
-
-print(arr1[1:now])
-print(arr2[1:now])
+# print(arr1[1:now])
+# print(arr2[1:now])
 
 for i in range(1,now):
     temp_first = first
@@ -54,3 +62,4 @@ for i in range(1,now):
         continue
 
 print(cnt)
+
