b99e48e47c2f724bdee5e42fd2afd2a95d8ff5fd
Jin409 <sualng123@gmail.com>
2022-04-04 15:14:59 +0900

    1449

--- BOJ/1166.py
+++
@@ -1,5 +0,0 @@
-n,l,w,h = map(int,input().split())
-#n 작은 박스 개수 l, w, h 큰 박스의 크기
-
-#a의 최댓값을 구하시오 
-big_size = l*w*h #큰 박스의 사이즈
--- BOJ/1449.py
+++ BOJ/1449.py
@@ -3,11 +3,29 @@
 # 테이프 좌우 0.5 만큼 간격을 줘야 한다.
 # 항승이가 필요한 테이프의 최소 개수 (겹쳐서 붙이기 가능)
 
+"""
+
+locas 라는 배열에 물이 새는 위치들을 넣어 오름차순으로 정렬 -> 왼쪽부터 샌다고 했으므로
+cnt = 1인 이유는 start 부터 시작하기 때문.
+간격을 0.5씩은 줘야 한다고 했으므로 0.5 * 2 = 1
+따라서 테이프 길이인 l-1 보다 
+
+"""
+
 n,l = map(int,input().split()) #물이 새는 곳의 개수와 테이프의 길이
 locas = list(map(int,input().split())) #물이 새는 위치들의 배열
-locas = locas.sort()
+locas = sorted(locas)
 
-for i in range(n):
-    diff = locas[i+1] - locas[i] #두 위치의 길이 차이를 계산.
-    if diff < l:
-        
+cnt = 1
+start = locas[0] #시작점
+
+for i in range(1,n):
+    end = locas[i]
+    if end - start <= l-1:
+        continue
+    else:
+        cnt+=1
+    start = end
+
+print(cnt)
+
