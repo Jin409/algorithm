4e0451b677d23c75ac206cd0b9c207928446300d
Jin409 <sualng123@gmail.com>
2022-04-07 23:32:50 +0900

    4/7 1564

--- BOJ/SilverII/1564.py
+++ BOJ/SilverII/1564.py
@@ -4,10 +4,21 @@ n = int(input())
 아래처럼 풀었는데 시간초과 남..!
 
 다시 풀어보기 .......
+다른 코드 참고함!
 """
-result = 1
+
+result =1
 for i in range(2,n+1):
     result*=i
+    temp_result = str(result)
+    while temp_result[-1]=="0":
+        result //=10
+        temp_result = str(result)
+        
+    result%=100000000000000000
+
+print(str(result)[-5:])
+
     
 # result = 1
 # for i in range(1,n+1):
