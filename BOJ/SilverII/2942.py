641f94f4bcaaa4302a782f6c0170aa1b40a7cd84
Jin409 <sualng123@gmail.com>
2022-04-14 00:08:12 +0900

    4/13 2942

--- BOJ/SilverII/2942.py
+++ BOJ/SilverII/2942.py
@@ -4,26 +4,36 @@ import math
 
 r,g = map(int,input().split())
 
-n = 0
+
 
 """
 약수 구하면 될 것 같음. 
 -> 계속 시간 초과가 나는중..
-
+최대공약수를 구할 때 루트만큼 시간이 걸리도록.
+20 의 약수는
+1 2 4 5 10 20 -> 대칭적인 형상을 띄기 때문에 루트 20인 4를 이용하여 계산
 """
 
-c_list =[]
+#최대공약수 반환
+def find_num(num):
+    num_list = []
+    sqrt_num = int(math.sqrt(num))
+    for i in range(1,sqrt_num+1):
+        if num%i==0:
+            num_list.append(i)
+            if num//i == i and i!=1:
+                continue 
+            else:
+                num_list.append(num//i)
+    return num_list
+
 
 min_num = min(r,g)
-min_num = int(math.sqrt(min_num))
+c_list = find_num(min_num) #공약수 리스트
+num_list = find_num(max(c_list))
 
-
-for i in range(1,min_num+1):
-    if g%i==0 and r%i==0:
-        c_list.append(i)
-
-c_list[-1]
-
-for i in range(len(c_list)):
-    print(f"{c_list[i]} {r//c_list[i]} {g//c_list[i]}")
+for i in range(len(num_list)):
+    num = num_list[i]
+    if r%num==0 and g%num==0:
+        print(f"{num_list[i]} {r//num_list[i]} {g//num_list[i]}")
 
